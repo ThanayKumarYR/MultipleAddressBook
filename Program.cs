4635a9410b397f21162def5ce3c77190b930c40a
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-02 11:13:40 +0530

    Refactoring code to add multiple address book

---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MultipleAddressBook
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Multiple Address Book !");
+            Dictionary<int,AddressBooking> dictionary = new Dictionary<int,AddressBooking>();
+            int BookIdMain = 1;
+            while (true) 
+            {
+                try
+                {
+                    Console.WriteLine("1.Create 2.Open 3.Rename 4.Display all 5.Display one 6.Delete 7.Exit");
+                    Console.Write("Enter the choice = ");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1: 
+                            Console.WriteLine("Creation of Address Book.");
+                            Console.Write("Enter the Address book name = ");
+                            string bookName  = Console.ReadLine();
+                            AddressBooking address = new AddressBooking(BookIdMain++, bookName);
+                            dictionary[address.BookId] = address;
+                            break;
+                        case 2:
+                            Console.WriteLine("Opening the AddressBook");
+                            Console.WriteLine("Press 1 -> Id or 2 -> Name");
+                            int pressNum = int.Parse(Console.ReadLine());
+                            switch (pressNum)
+                            { 
+                                case 1:
+                                    Console.Write("Enter the id = ");
+                                    int Bid = int.Parse(Console.ReadLine());
+                                    if (dictionary.ContainsKey(Bid))
+                                        dictionary[Bid].Open();
+                                    else Console.WriteLine("Id does not exist !");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the Bookname = ");
+                                    string BookNamed = Console.ReadLine();
+                                    if (dictionary.Values.Any((book) => book.Bookname == BookNamed))
+                                    {
+                                        dictionary.Values.Single((book) => book.Bookname == BookNamed).Open();
+                                    }
+                                    else
+                                    { 
+                                        Console.WriteLine("Book name doesnot exist !");
+                                    }
+                                    break;
+                                default: 
+                                    Console.WriteLine("Invalid Input !");
+                                    break;
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine("Renaming the AddressBook");
+                            Console.WriteLine("Press 1 -> Id or 2 -> Name");
+                            int pressNum1 = int.Parse(Console.ReadLine());
+                            switch (pressNum1)
+                            {
+                                case 1:
+                                    Console.Write("Enter the id = ");
+                                    int Bid = int.Parse(Console.ReadLine());
+                                    if (dictionary.ContainsKey(Bid))
+                                    {
+                                        Console.Write("Rename the book= ");
+                                        string BookReName = Console.ReadLine();
+                                        dictionary[Bid].Bookname = BookReName;
+                                    }
+                                    else Console.WriteLine("Id does not exist !");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the Bookname = ");
+                                    string BookNamed = Console.ReadLine();
+                                    if (dictionary.Values.Any((book) => book.Bookname == BookNamed))
+                                    {
+                                        Console.Write("Rename the book= ");
+                                        string BookReName = Console.ReadLine();
+                                        dictionary.Values.Single((book) => book.Bookname == BookNamed).Bookname = BookReName;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Book name doesnot exist !");
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid Input !");
+                                    break;
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Displaying all the Address Book");
+                            foreach (var item in dictionary)
+                            {
+                                Console.WriteLine($"Id = {item.Key}, Book Name = {item.Value.Bookname}");
+                            }
+                            break;
+                        case 5:
+                            Console.WriteLine("Displaying the AddressBook");
+                            Console.WriteLine("Press 1 -> Id or 2 -> Name");
+                            int pressNum2 = int.Parse(Console.ReadLine());
+                            switch (pressNum2)
+                            {
+                                case 1:
+                                    Console.Write("Enter the id = ");
+                                    int Bid = int.Parse(Console.ReadLine());
+                                    if (dictionary.ContainsKey(Bid))
+                                    {
+                                        Console.WriteLine($"Book Id = {dictionary[Bid].BookId}");
+                                        Console.WriteLine($"Book Name = {dictionary[Bid].Bookname}");
+                                        foreach (Contacts contact in dictionary[Bid].list) contact.display();
+                                    }
+                                    else Console.WriteLine("Id does not exist !");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the Bookname = ");
+                                    string BookNamed = Console.ReadLine();
+                                    if (dictionary.Values.Any((book) => book.Bookname == BookNamed))
+                                    {
+                                       int BookID = dictionary.Values.Single((book) => book.Bookname == BookNamed).BookId;
+                                        Console.WriteLine($"Book Id = {dictionary[BookID].BookId}");
+                                        Console.WriteLine($"Book Name = {dictionary[BookID].Bookname}");
+                                        foreach (Contacts contact in dictionary[BookID].list) contact.display();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Book name doesnot exist !");
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid Input !");
+                                    break;
+                            }
+                            break;
+                        case 6:
+                            Console.WriteLine("Deleting the AddressBook");
+                            Console.WriteLine("Press 1 -> Id or 2 -> Name");
+                            int pressNum3 = int.Parse(Console.ReadLine());
+                            switch (pressNum3)
+                            {
+                                case 1:
+                                    Console.Write("Enter the id = ");
+                                    int Bid = int.Parse(Console.ReadLine());
+                                    if (dictionary.ContainsKey(Bid))
+                                    {
+                                        Console.WriteLine($"Removing address book in {dictionary[Bid].Bookname}");
+                                        dictionary.Remove(Bid);
+                                    }
+                                    else Console.WriteLine("Id does not exist !");
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the Bookname = ");
+                                    string BookNamed = Console.ReadLine();
+                                    if (dictionary.Values.Any((book) => book.Bookname == BookNamed))
+                                    {
+                                        int BookID = dictionary.Values.Single((book) => book.Bookname == BookNamed).BookId;
+                                        Console.WriteLine($"Removing address book in {dictionary[BookID].Bookname}");
+                                        dictionary.Remove(BookID);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Book name does not exist !");
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid Input !");
+                                    break;
+                            }
+                            break;
+                        case 7:
+                            return;
+
+                        default:
+                            Console.WriteLine("Invalid Input !");
+                            break ;
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine("\n\t\t\t" + ex.Message + "\n" );
+                }
+            }
+        }
+    }
+}
